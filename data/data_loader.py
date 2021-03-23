4cbc0e75016b85be5915a416631772e5b34de3d6
cookieminions <1095715895@qq.com>
2021-03-23 18:51:55 +0800

    update Dataset_Pred

--- data/data_loader.py
+++ data/data_loader.py
@@ -15,7 +15,7 @@ class Dataset_ETT_hour(Dataset):
     def __init__(self, root_path, flag='train', size=None, 
                  features='S', data_path='ETTh1.csv', 
                  target='OT', scale=True, timeenc=0, freq='h'):
-        # size [seq_len, label_len pred_len]
+        # size [seq_len, label_len, pred_len]
         # info
         if size == None:
             self.seq_len = 24*4*4
@@ -102,7 +102,7 @@ class Dataset_ETT_minute(Dataset):
     def __init__(self, root_path, flag='train', size=None, 
                  features='S', data_path='ETTm1.csv', 
                  target='OT', scale=True, timeenc=0, freq='t'):
-        # size [seq_len, label_len pred_len]
+        # size [seq_len, label_len, pred_len]
         # info
         if size == None:
             self.seq_len = 24*4*4
@@ -192,7 +192,7 @@ class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None, 
                  features='S', data_path='ETTh1.csv', 
                  target='OT', scale=True, timeenc=0, freq='h'):
-        # size [seq_len, label_len pred_len]
+        # size [seq_len, label_len, pred_len]
         # info
         if size == None:
             self.seq_len = 24*4*4
@@ -224,8 +224,8 @@ class Dataset_Custom(Dataset):
         '''
         df_raw.columns: ['date', ...(other features), target feature]
         '''
-        cols = list(df_raw.columns); cols.remove(self.target)
-        df_raw = df_raw[cols+[self.target]]
+        cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
+        df_raw = df_raw[['date']+cols+[self.target]]
 
         num_train = int(len(df_raw)*0.7)
         num_test = int(len(df_raw)*0.2)
@@ -279,6 +279,98 @@ class Dataset_Custom(Dataset):
     
     def __len__(self):
         return len(self.data_x) - self.seq_len- self.pred_len + 1
+
+    def inverse_transform(self, data):
+        return self.scaler.inverse_transform(data)
+
+class Dataset_Pred(Dataset):
+    def __init__(self, root_path, flag='pred', size=None, 
+                 features='S', data_path='ETTh1.csv', 
+                 target='OT', scale=True, timeenc=0, freq='15min'):
+        # size [seq_len, label_len, pred_len]
+        # info
+        if size == None:
+            self.seq_len = 24*4*4
+            self.label_len = 24*4
+            self.pred_len = 24*4
+        else:
+            self.seq_len = size[0]
+            self.label_len = size[1]
+            self.pred_len = size[2]
+        # init
+        assert flag in ['pred']
+        
+        self.features = features
+        self.target = target
+        self.scale = scale
+        self.timeenc = timeenc
+        self.freq = freq
+
+        self.root_path = root_path
+        self.data_path = data_path
+        self.__read_data__()
+
+    def __read_data__(self):
+        self.scaler = StandardScaler()
+        df_raw = pd.read_csv(os.path.join(self.root_path,
+                                          self.data_path))
+        '''
+        df_raw.columns: ['date', ...(other features), target feature]
+        '''
+        cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
+        df_raw = df_raw[['date']+cols+[self.target]]
+        
+        border1 = len(df_raw)-self.seq_len
+        border2 = len(df_raw)
+        
+        if self.features=='M' or self.features=='MS':
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        elif self.features=='S':
+            df_data = df_raw[[self.target]]
+
+        if self.scale:
+            self.scaler.fit(df_data.values)
+            data = self.scaler.transform(df_data.values)
+        else:
+            data = df_data.values
+            
+        tmp_stamp = df_raw[['date']][border1:border2]
+        tmp_stamp['date'] = pd.to_datetime(tmp_stamp.date)
+        pred_dates = pd.date_range(tmp_stamp.date.values[-1], periods=self.pred_len+1, freq=self.freq)
+        
+        df_stamp = pd.DataFrame(columns = ['date'])
+        df_stamp.date = list(tmp_stamp.date.values) + list(pred_dates[1:])
+        
+        if self.timeenc==0:
+            df_stamp['month'] = df_stamp.date.apply(lambda row:row.month,1)
+            df_stamp['day'] = df_stamp.date.apply(lambda row:row.day,1)
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row:row.weekday(),1)
+            df_stamp['hour'] = df_stamp.date.apply(lambda row:row.hour,1)
+            data_stamp = df_stamp.drop(['date'],1).values
+        elif self.timeenc==1:
+            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq[-1:])
+            data_stamp = data_stamp.transpose(1,0)
+
+        self.data_x = data[border1:border2]
+        self.data_y = data[border1:border2]
+        self.data_stamp = data_stamp
+    
+    def __getitem__(self, index):
+        s_begin = index
+        s_end = s_begin + self.seq_len
+        r_begin = s_end - self.label_len
+        r_end = r_begin + self.label_len + self.pred_len
+
+        seq_x = self.data_x[s_begin:s_end]
+        seq_y = self.data_y[r_begin:r_begin+self.label_len]
+        seq_x_mark = self.data_stamp[s_begin:s_end]
+        seq_y_mark = self.data_stamp[r_begin:r_end]
+
+        return seq_x, seq_y, seq_x_mark, seq_y_mark
+    
+    def __len__(self):
+        return len(self.data_x) - self.seq_len + 1
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
