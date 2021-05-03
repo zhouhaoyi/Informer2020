04aaa8081c13f2caeca4ec77df0d80933b586f50
Cookie <1095715895@qq.com>
2021-05-03 16:58:54 +0800

    update dec mix attn

--- models/model.py
+++ models/model.py
@@ -85,7 +85,7 @@ class InformerStack(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
                 factor=5, d_model=512, n_heads=8, e_layers=[3,2,1], d_layers=2, d_ff=512, 
                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
-                output_attention = False, distil=True,
+                output_attention = False, distil=True, mix=True,
                 device=torch.device('cuda:0')):
         super(InformerStack, self).__init__()
         self.pred_len = out_len
