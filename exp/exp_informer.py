7fcb5ccf11586439b5d06f19508ecbcda41e83e9
Cookie <1095715895@qq.com>
2021-05-07 13:48:41 +0800

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -80,32 +80,18 @@ class Exp_Informer(Exp_Basic):
             Data = Dataset_Pred
         else:
             shuffle_flag = True; drop_last = True; batch_size = args.batch_size; freq=args.freq
-        if args.data=='custom':
-            data_set = Data(
-                root_path=args.root_path,
-                cols=args.cols,
-                data_path=args.data_path,
-                flag=flag,
-                size=[args.seq_len, args.label_len, args.pred_len],
-                features=args.features,
-                target=args.target,
-                inverse=args.inverse,
-                timeenc=timeenc,
-                freq=freq
-            )
-        else:
-            data_set = Data(
-                root_path=args.root_path,
-                
-                data_path=args.data_path,
-                flag=flag,
-                size=[args.seq_len, args.label_len, args.pred_len],
-                features=args.features,
-                target=args.target,
-                inverse=args.inverse,
-                timeenc=timeenc,
-                freq=freq
-            )
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            features=args.features,
+            target=args.target,
+            inverse=args.inverse,
+            timeenc=timeenc,
+            freq=freq,
+            cols=args.cols
+        )
         print(flag, len(data_set))
         data_loader = DataLoader(
             data_set,
