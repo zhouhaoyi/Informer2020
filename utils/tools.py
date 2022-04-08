6d06e2528d6c96ae133204587049d8f4bf451ba2
MountVoom <491868399@qq.com>
2022-04-08 14:33:06 +0800

    fix MS inverse

--- utils/tools.py
+++ utils/tools.py
@@ -70,4 +70,7 @@ class StandardScaler():
     def inverse_transform(self, data):
         mean = torch.from_numpy(self.mean).type_as(data).to(data.device) if torch.is_tensor(data) else self.mean
         std = torch.from_numpy(self.std).type_as(data).to(data.device) if torch.is_tensor(data) else self.std
+        if data.shape[-1] != mean.shape[-1]:
+            mean = mean[-1:]
+            std = std[-1:]
         return (data * std) + mean
